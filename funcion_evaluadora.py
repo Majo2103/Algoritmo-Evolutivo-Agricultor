81bbc0cb2161f8e9b1c32452f5e115c64943a9f2
María José Velázquez <mariajosevelazquez@MacBook-Pro-de-Maria.local>
2024-04-26 15:05:40 -0600

    empece funcion probabilidad de crecimiento

--- funcion_evaluadora.py
+++ funcion_evaluadora.py
@@ -18,17 +18,19 @@ def evaluar(genotipo:Genotipo = [], m2 = int, sol = str, agua = str, temp = str)
      # calculamos los costos
      costo = 0
      # El precio aproximado de cuidar un m2 por mes de varios cultivos es presentado en https://www.scielo.sa.cr/pdf/ac/v44n2/0377-9424-ac-44-02-81.pdf
+     #Suponemos que la mano de
      base_mano_obra = 400 # Moderado /m2
-     if cultivo.mano_obra == 'Bajo' #/m2
+     if cultivo.mano_obra == 'Bajo' #/m2 
         base_mano_obra = base_mano_obra * 0.5
      if cultivo.mano_obra == 'Alto' #/m2
         base_mano_obra = base_mano_obra * 1.5
+
      costo_mano_obra= base_mano_obra * m2
 
      costo += costo_mano_obra
-     
 
-     probabilidad = 1
+
+     probabilidad = probabilidad_crecimiento(cultivo,sol,agua,temp) #usamos una función externa
      #encontrar una forma de calcular la probabilidad de que crezca el cultivo dado el status quo plantagi
 
 
@@ -37,5 +39,35 @@ def evaluar(genotipo:Genotipo = [], m2 = int, sol = str, agua = str, temp = str)
      utilidad_total += utilidad
   
   
-
   return utilidad
+
+
+
+
+
+def probabilidad_crecimiento(cultivo = object, sol = str, agua = str, temp = str):
+   prob=1
+
+#como el agua afecta la probabilidad
+   if (agua != cultivo.agua):
+      
+      if (cultivo.agua == 'Alta'):
+         if (agua == 'Baja'):
+            prob = prob * .3 #ajustar de acuerdo a observaciones
+         if (agua == 'Moderada'):
+            prob = prob * .7 #ajustar de acuerdo a observaciones
+
+      if (cultivo.agua == 'Baja'):
+         if (agua == 'Alta'):
+            prob = prob * .3 #ajustar de acuerdo a observaciones
+         if (agua == 'Moderada'):
+            prob = prob * .7 #ajustar de acuerdo a observaciones
+      
+      if (cultivo.agua == 'Moderada'):
+            prob = prob * .7 #ajustar de acuerdo a observaciones
+
+#como la temporada afecta la probabilidad
+   if (temp != cultivo.temp):
+       prob=prob*.6     
+
+   return prob 
